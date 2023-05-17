86a87fa899e6b2a44f67ba2c3f501df759f3655e
Milan Tagline <milanc.tagline@gmail.com>
2023-05-17 17:14:46 +0530

    Changed in Junit1 File

--- src/Junit/Junit1.java
+++ src/Junit/Junit1.java
@@ -20,4 +20,16 @@ import org.openqa.selenium.chrome.ChromeDriver;
         driver.findElement(By.xpath("//input[@name=\"password\"]")).sendKeys("test@11");
         driver.findElement(By.xpath("//button[@type=\"submit\"]")).click();
     }
+    public void LoginTest2()//Newly added
+    {
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+
+        driver.get("http://sdash-development.s3-website.us-east-2.amazonaws.com/");
+
+        driver.findElement(By.xpath("//div[@title='Sales']")).click(); //-------------Sales
+        driver.findElement(By.xpath("//input[@name=\"email\"]")).sendKeys("test1@yopmail.com");
+        driver.findElement(By.xpath("//input[@name=\"password\"]")).sendKeys("test@11");
+        driver.findElement(By.xpath("//button[@type=\"submit\"]")).click();
+    }
 }
